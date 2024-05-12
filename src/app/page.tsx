16b60475aebe96e5a8195fff50d8461ea986c6c3
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-05-12 16:58:26 +0300

    Fix margins & paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,27 @@ export default function Home() {
       <div className={ styles.container }>
         <div className={ styles.navigation }>
           <p>даена</p>
-          <Link className={ styles.music } href="/music">музыка</Link>
+          <Link className={ styles.music } href="/music">дискография</Link>
         </div>
 
         <div className={ styles.socials }>
           <a href="https://vk.com/daethisyou" target="_blank">
-            <img src="icons/vk.svg" alt="vk" width="30px"/>
+            <img src="icons/vk.svg" alt="vk" width="35px"/>
           </a>
           <a href="https://www.instagram.com/whosdaena/" target="_blank">
-            <img src="icons/insta.svg" alt="insta" width="30px"/>
+            <img src="icons/insta.svg" alt="insta" width="35px"/>
           </a>
           <a href="https://www.youtube.com/@whosdaena" target="_blank">
-            <img src="icons/youtube.svg" alt="youtube" width="30px"/>
+            <img src="icons/youtube.svg" alt="youtube" width="35px"/>
           </a>
           <a href="https://music.yandex.ru/artist/8979279" target="_blank">
-            <img src="icons/yamusic.svg" alt="yamusic" width="30px"/>
+            <img src="icons/yamusic.svg" alt="yamusic" width="35px"/>
           </a>
           <a href="https://open.spotify.com/artist/65J0xSmKmX3ENY7JYyTAUQ" target="_blank">
-            <img src="icons/spotify.svg" alt="spotify" width="30px"/>
+            <img src="icons/spotify.svg" alt="spotify" width="35px"/>
           </a>
           <a href="https://music.apple.com/ru/artist/%D0%B4%D0%B0%D0%B5%D0%BD%D0%B0/1553607758" target="_blank">
-            <img src="icons/itunes.svg" alt="itunes" width="30px"/>
+            <img src="icons/itunes.svg" alt="itunes" width="35px"/>
           </a>
         </div>
       </div>
