2c470c951ba69c1523a97d95c50256c2519092c4
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-10 16:27:12 +0300

    Added animation to page load

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,14 +5,15 @@ import { motion } from 'framer-motion'
 export default function Transition({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ y: -10, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{
+      initial={ { y: -10, opacity: 0 } }
+      animate={ { y: 0, opacity: 1 } }
+      exit={ { y: 10, opacity: 0 } }
+      transition={ {
         ease: 'easeInOut',
         duration: 0.5
-      }}
+      } }
     >
-      {children}
+      { children }
     </motion.div>
   )
 }
