cb041ebe0c4fad3d8e783df05ab157c5c0205096
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-19 05:09:06 +0300

    Minor UI improvements

--- app/page.tsx
+++ app/page.tsx
@@ -16,27 +16,31 @@ import { socialsList } from '@/data/socials'
 
 export default function Home() {
   return (
-    <main className="relative h-dvh w-full overflow-hidden">
+    <main className="fixed h-dvh w-full touch-none overflow-hidden">
       <header className="absolute left-[50%] top-0 z-10 flex w-full max-w-screen-2xl translate-x-[-50%] justify-between px-6 pt-6 text-2xl lg:px-8 lg:pt-8 lg:text-3xl">
         <h1 className="cursor-default font-bold">даена</h1>
-        <div className="flex flex-col items-end gap-2 lg:gap-4">
-          <p>
-            <Link className="link-animation" href="/discography">
-              дискография
-            </Link>
-          </p>
-          <p>
-            <Link className="link-animation" href="/video">
-              видео
-            </Link>
-          </p>
+        <div className="flex flex-col items-end gap-3 lg:gap-4">
+          <Link
+            className="link-animation transition active:scale-95 lg:active:transform-none"
+            href="/discography"
+          >
+            дискография
+          </Link>
+          <Link
+            className="link-animation transition active:scale-95 lg:active:transform-none"
+            href="/video"
+          >
+            видео
+          </Link>
         </div>
       </header>
 
       <Drawer>
         <DrawerTrigger className="absolute bottom-6 left-[50%] z-10 translate-x-[-50%] text-xl outline-none lg:text-2xl">
           <ChevronUp className="mx-auto" />
-          <p className="link-animation">слушать</p>
+          <p className="link-animation transition active:scale-95 lg:active:transform-none">
+            слушать
+          </p>
         </DrawerTrigger>
         <DrawerContent className="border-none bg-black/50 outline-none">
           <DrawerHeader className="flex flex-col items-center justify-center">
@@ -45,7 +49,7 @@ export default function Home() {
               Слушать на всех площадках
             </DrawerDescription>
           </DrawerHeader>
-          <DrawerFooter className="flex gap-3 lg:w-full lg:flex-row">
+          <DrawerFooter className="flex gap-3 pt-0 lg:w-full lg:flex-row">
             {socialsList.map(social => (
               <Link className="lg:flex-grow" href={social.href} target="_blank" key={social.alt}>
                 <Button className="flex w-full gap-2 py-6 transition hover:bg-white/15 lg:active:scale-[95%]">
